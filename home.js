8f7dc2593c3ad2d231be4cf13df744cb69a918e1
ranfomshi <66889344+ranfomshi@users.noreply.github.com>
2022-08-15 10:07:47 +0100

    initial upload

---
+++ home.js
@@ -0,0 +1,24 @@
+var direction = 'down'
+var x = 80
+function scrollEffect() {
+
+
+    if (x >= 80) {
+        direction = 'down'
+    }
+    if (x <= 20) {
+        direction = 'up'
+    }
+    if (direction == 'down') {
+        x = x - 0.5
+    } else {
+        x = x + 0.5
+    }
+
+    
+
+
+    document.getElementById('content').style.background =
+        'linear-gradient(to right, rgba(255, 0, 0,' + (x / 100).toFixed(2) + ' ), rgba(0,0,255,' + (x / 100).toFixed(2) +
+        '))'
+}
