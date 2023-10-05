361b4d11dd94cf42c468b8c23a24dbbb553465cc
Stuart Pecksen <stuart151087@gmail.com>
2023-10-05 16:38:13 +0100

    home screen bacground

--- home.js
+++ home.js
@@ -1,26 +1,6 @@
 var direction = 'down'
 var x = 0
-function scrollEffect() {
 
-
-    if (x >= 80) {
-        direction = 'down'
-    }
-    if (x <= 20) {
-        direction = 'up'
-    }
-    if (direction == 'down') {
-        x = x - 0.5
-    } else {
-        x = x + 0.5
-    }
-
-    
-
-
-    document.getElementById('content').style.background =
-        '#556677'
-}
 
 function sendEvent(category, action, label) {
     if ("ga" in window) {
