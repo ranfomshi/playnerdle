a688f96f07102a5c1835eea47acffbe90a6bfe77
Stuart Pecksen <stuart151087@gmail.com>
2025-04-23 14:39:47 +0100

    .

--- home.js
+++ home.js
@@ -1,37 +1,32 @@
-var direction = 'down'
-var x = 0
-
-
 function sendEvent(category, action, label) {
     if ("ga" in window) {
-        tracker = ga.getAll()[0];
-        if (tracker)
+        const tracker = ga.getAll()[0];
+        if (tracker) {
             tracker.send("event", category, action, label);
+        }
     }
-
 }
 
-function nerdleClick(e) {
-    sendEvent('homeSelection', 'Click', 'nerdle')
-}
-function reactionClick(e) {
-    sendEvent('homeSelection', 'Click', 'reaction')
-}
-function colourMatchClick(e) {
-    sendEvent('homeSelection', 'Click', 'colourMatch')
-}
-function bludleClick(e) {
-    sendEvent('homeSelection', 'Click', 'bludle')
-}
-function codleClick(e) {
-    sendEvent('homeSelection', 'Click', 'codle')
-}
-function huntClick(e) {
-    sendEvent('homeSelection', 'Click', 'hunt')
-}
-function guessHueClick(e) {
-    sendEvent('homeSelection', 'Click', 'guesshue')
-}
-function alternateClick(e) {
-    sendEvent('homeSelection', 'Click', 'alternate')
-}
+const trackClick = (gameName) => {
+    sendEvent('homeSelection', 'Click', gameName);
+};
+
+const tileHandlers = {
+    nerdle: () => trackClick('nerdle'),
+    reaction: () => trackClick('reaction'),
+    colourMatch: () => trackClick('colourMatch'),
+    bludle: () => trackClick('bludle'),
+    codle: () => trackClick('codle'),
+    hunt: () => trackClick('hunt'),
+    guesshue: () => trackClick('guesshue'),
+    alternate: () => trackClick('alternate'),
+    connex: () => trackClick('connex'),
+    heardle: () => trackClick('heardle'),
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+    Object.entries(tileHandlers).forEach(([id, handler]) => {
+        const el = document.getElementById(id);
+        if (el) el.addEventListener("click", handler);
+    });
+});
