e71c67692b5c5b1495123fdd06aa7a1a91e7c2e9
ranfomshi <66889344+ranfomshi@users.noreply.github.com>
2026-04-02 16:42:28 +0100

    Add Snoules game page and site navigation links

--- globalNav/navLinks.js
+++ globalNav/navLinks.js
@@ -14,5 +14,6 @@ export const navLinks = [
     { name: "tintuition", href: "/tintuition" },
     { name: "connex", href: "/connex" },
     { name: "seequence", href: "/seequence" },
-    { name: "word mash", href: "/wordmash" }
+    { name: "word mash", href: "/wordmash" },
+    { name: "snoules", href: "/snoules" }
 ];
